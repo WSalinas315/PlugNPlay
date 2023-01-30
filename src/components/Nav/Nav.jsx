b48293af4d3374f0c74c7ec85d85102222ec1ff2
August McAllister <august.chm@gmail.com>
2023-01-30 13:23:18 -0600

    setup: cleaned up nav component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,49 +5,42 @@ import "./Nav.css";
 
 function Nav() {
   const user = useSelector((store) => store.user);
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
 
   return (
     <div className="nav">
       <nav>
         <ul>
           <li>
-            <Link to="/home">
-              <h2 className="nav-title">Prime Solo Project</h2>
+            {!user.id && (
+              <Link className="navLink" to="/login">
+                Login / Register
+              </Link>
+            )}
+          </li>
+
+          {user.id && (
+            <>
+              <li>
+                <Link className="navLink" to="/user">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <button
+                  className="navLink"
+                  onClick={() => dispatch({ type: "LOGOUT" })}
+                >
+                  Log Out
+                </button>
+              </li>
+            </>
+          )}
+          <li>
+            <Link className="navLink" to="/about">
+              About
             </Link>
           </li>
-          <div>
-            <li>
-              {!user.id && (
-                <Link className="navLink" to="/login">
-                  Login / Register
-                </Link>
-              )}
-            </li>
-
-            {user.id && (
-              <>
-                <li>
-                  <Link className="navLink" to="/user">
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <button
-                    className="navLink"
-                    onClick={() => dispatch({ type: "LOGOUT" })}
-                  >
-                    Log Out
-                  </button>
-                </li>
-              </>
-            )}
-            <li>
-              <Link className="navLink" to="/about">
-                About
-              </Link>
-            </li>
-          </div>
         </ul>
       </nav>
     </div>
