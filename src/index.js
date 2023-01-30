100f4d46a548ba45116fb720acea4844c4656c96
traceytreat <treatracey@gmail.com>
2023-01-30 14:04:23 -0600

    header plus logo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
+import Header from './components/Header/Header';
 import store from './redux/store';
 
 import App from './components/App/App';
 
 ReactDOM.render(
   <Provider store={store}>
+    <Header />
     <App />
   </Provider>,
   document.getElementById('react-root'),
