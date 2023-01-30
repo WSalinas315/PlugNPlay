f39a7f8165d8ddc2ee808d64ef83f77c455fac6f
August McAllister <august.chm@gmail.com>
2023-01-30 15:40:01 -0600

    refactor: comments to elucidate /byTags endpoint

--- server/routes/rawg.router.js
+++ server/routes/rawg.router.js
@@ -14,7 +14,7 @@ const keyUrl = ("key=" + key);
   main recommendations
   game by ID √
   games by genre √
-  games by tag
+  games by tag √
   games by name
 */
 
@@ -59,26 +59,31 @@ router.get('/byTags', async (req, res) => {
     })
   }
 
+  //* filters out non-english tags, isolates the tag name, and sorts alphabetically
   const tagFilter = (game) => {
-    //* filters out non-english tags, isolates the tag name, and sorts alphabetically
     const gameTags =
       game.tags
         .filter(tag => tag.language === "eng")
         .map(tag => tag.name)
         .sort()
 
-    //* isolate properties we want to see
+    //* returns game object, with tags filtered
     return {
-      name: game.name,
-      id: game.id,
-      metacritic: game.metacritic,
+      ...game,
       tags: gameTags.map(tag => tag.toLowerCase())
     }
+  }
+
+  //* sorts games in order of most matched tags to fewest
+  const sortByTagRelevance = (a, b) => {
+    return b.tags.filter(tag => userTags.includes(tag.toLowerCase())).length -
+      a.tags.filter(tag => userTags.includes(tag.toLowerCase())).length
   }
 
   // QUERY
   try {
 
+    //TODO: fix query throwing 404 if not all pages populate
     for (let tag of userTags) {
       searchQueries.push(axios.get(`https://api.rawg.io/api/games?${keyUrl}&tags=${tag.toLowerCase()}&page_size=40`))
       searchQueries.push(axios.get(`https://api.rawg.io/api/games?${keyUrl}&tags=${tag.toLowerCase()}&page_size=40&page=2`))
@@ -89,18 +94,14 @@ router.get('/byTags', async (req, res) => {
 
     const taggedGames =
       taggedGameObjects
-        .map(obj => obj.data.results)
-        .flat()
+        .map(obj => obj.data.results) //* isolates the actual API results
+        .flat() //* flattens the results into a single one-dimensional array
         .filter(dupeFilter)
         .map(tagFilter)
-        .sort((a, b) => {
-          return b.tags.filter(tag => userTags.includes(tag.toLowerCase())).length -
-            a.tags.filter(tag => userTags.includes(tag.toLowerCase())).length
-        })
-
+        .sort(sortByTagRelevance)
+    
     res.send(taggedGames)
   } catch (err) {
-
     console.log(err)
     res.sendStatus(500)
   }
