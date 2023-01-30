f74942bdcfe905ffa5ec70a5a83bc380d8293ca7
Billy Salinas <wsalinas315@gmail.com>
2023-01-30 14:08:00 -0600

    Games Router Wishlist GET written

--- server/routes/game.router.js
+++ server/routes/game.router.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const {rejectUnauthenticated} = require('../modules/authentication-middleware');
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-  // GET route code here
+// Wishlist - GET
+router.get('/:id', rejectUnauthenticated, async (req, res) => {
+  // console.log('In game router: Wishlist - GET');
+  try{
+    const userID = req.params.id;
+    const wishlistResult = await pool.query(`SELECT * FROM "wishlist" WHERE "user_id" = $1;`, [userID]);
+    res.send(wishlistResult);
+  } catch (err) {
+    console.log('Game Router Wishlist GET error:', err);
+    res.sendStatus(500);
+  }
 });
 
 /**
