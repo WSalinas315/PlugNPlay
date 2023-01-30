eb935264a79a15ee4dbd38c90a94c86e33751a59
traceytreat <treatracey@gmail.com>
2023-01-30 14:07:32 -0600

    change header location

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import Header from './components/Header/Header';
 import store from './redux/store';
 
 import App from './components/App/App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Header />
     <App />
   </Provider>,
   document.getElementById('react-root'),
