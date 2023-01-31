b6d4a50ecf5b4c8a785877db5a5be74109081652
August McAllister <august.chm@gmail.com>
2023-01-31 11:30:07 -0600

    feat: added api/tags routes

--- server/routes/tags.router.js
+++ server/routes/tags.router.js
@@ -2,18 +2,33 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-  // GET route code here
+// ==========================================================================================
+// GET ALL TAGS
+
+router.get('/', async (req, res) => {
+  try {
+    const { data: tagResults } = await pool.query(`SELECT * FROM tag`)
+    res.send(tagResults);
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
 });
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-  // POST route code here
-});
+// ==========================================================================================
+// GET TAG BY ID
+
+router.get('/byID/:id', async (req, res) => {
+
+  const { id } = req.params
+
+  try {
+    const { data: tag } = await pool.query(`SELECT * FROM tag WHERE id = $1`, [ id ])
+    res.send(tag)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
+})
 
 module.exports = router;
