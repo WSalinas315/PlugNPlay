b7b1cb1b766b1c2b289e4d44036eb6f217420182
Billy Salinas <wsalinas315@gmail.com>
2023-01-31 11:36:20 -0600

    renamed viewrecommended function and imported react

--- src/components/ViewRecommended/ViewRecommended.jsx
+++ src/components/ViewRecommended/ViewRecommended.jsx
@@ -1,5 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
-export default function () {
+export default function ViewRecommendation() {
 
   return (<></>)
 }
