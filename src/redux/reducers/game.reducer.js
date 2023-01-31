0245967df71207ca8ac77bbdc9c2c32e464349d8
August McAllister <august.chm@gmail.com>
2023-01-31 14:01:01 -0600

    feat: wrote "game" reducers

---
+++ src/redux/reducers/game.reducer.js
@@ -0,0 +1,52 @@
+import { combineReducers } from "redux"
+
+const currentlyViewedGame = (state = {}, { type, payload }) => {
+  return {
+    'GAME/SET_CURRENT': payload,
+    'GAME/CLEAR_CURRENT': {}
+  }[type] || state
+}
+
+const searchResults = (state = [], { type, payload }) => {
+  return {
+    'GAME/SET_SEARCH_RESULTS': payload,
+    'GAME/CLEAR_SEARCH_RESULTS': []
+  }[type] || state;
+}
+
+const recommendations = (state = [], { type, payload }) => {
+  return {
+    'GAME/SET_RECOMMENDATIONS': payload,
+    'GAME/CLEAR_RECOMMENDATIONS': []
+  }[type] || state;
+}
+
+const userWishlist = (state = [], { type, payload }) => {
+  return {
+    'GAME/SET_WISHLIST': payload,
+    'GAME/CLEAR_WISHLIST': []
+  }[type] || state;
+}
+
+const userIgnorelist = (state = [], { type, payload }) => {
+  return {
+    'GAME/SET_IGNORELIST': payload,
+    'GAME/CLEAR_IGNORELIST': []
+  }[type] || state;
+}
+
+const userPlayedList = (state = [], { type, payload }) => {
+  return {
+    'GAME/SET_PLAYED': payload,
+    'GAME/CLEAR_PLAYED': []
+  }[type] || state;
+}
+
+export default combineReducers({
+  currentlyViewedGame,
+  searchResults,
+  recommendations,
+  userWishlist,
+  userIgnorelist,
+  userPlayedList,
+})
