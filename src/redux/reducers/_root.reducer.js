f77d433707bcef92f43fb18a511ffdc92e033280
August McAllister <august.chm@gmail.com>
2023-01-31 14:26:19 -0600

    feat: wrote rawg sagas

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
-import games from './game.reducer'
+import games from './game.reducer';
 
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
