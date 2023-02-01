f1de7d297daf0100a056a6a21ccf637d85633268
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2023-02-01 13:14:24 -0600

    added picture

--- src/components/ViewProfile/ViewProfile.jsx
+++ src/components/ViewProfile/ViewProfile.jsx
@@ -7,6 +7,9 @@ function ViewProfile () {
     <p> Full Name</p>
     <p> Username</p>
     <p>Email</p>
+    <p> Avatar</p>
+    <div id="picture" className= "container">
+  </div>
   </div>
   )
 }
