992c0bb75664c17cf33ea706312df581a45148cf
Billy Salinas <wsalinas315@gmail.com>
2023-02-01 10:54:50 -0600

    New SwipeBox component created and a swipe box now renders to the Recommendation View

--- src/components/ViewRecommended/ViewRecommended.jsx
+++ src/components/ViewRecommended/ViewRecommended.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import SwipeBox from '../SwipeBox/SwipeBox';
 
 export default function ViewRecommendation() {
 
   return (
     <>
-      
+      {/* Placeholder text */}
       <h1>🎮 RECOMMENDATION 🕹 VIEW</h1>
-      
+      <br />
+
+      {/* I think props for the functions to call on different swipe directions will need
+      to be passed down into the SwipeBox component */}
+      <SwipeBox />
+
     </>
   )
 }
