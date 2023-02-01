418f9dbaff774349a43a0112a2c2500872d8de48
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2023-02-01 15:36:34 -0600

    added defualt avatar as well as a save button

--- src/components/ViewProfile/ViewProfile.jsx
+++ src/components/ViewProfile/ViewProfile.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 
 function ViewProfile () {
+  //const profileImage = require("./basic.jpeg");
   return (
     <div className="container">
     <h2> Profile</h2>
+
+    <div><img src='images/defualt.png' /></div>
+
     <p> Full Name</p>
+    <input placeholder="full name" ></input>
+
     <p> Username</p>
+    <input placeholder="username" ></input>
+
     <p>Email</p>
-    <p> Avatar</p>
+    <input placeholder="email" ></input>
+
     <div id="picture" className= "container">
   </div>
-  <button> edit</button>
+
+  <button> Edit </button>
+  
+  <button> Save </button>
   
 
   </div>
