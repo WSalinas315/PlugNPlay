b3812c776dc043204f5d39d12b3baa4aff0d21e6
August McAllister <august.chm@gmail.com>
2023-02-02 14:44:49 -0600

    chore: added autofill button to SurveyPage

--- src/components/SurveyPage/SurveyPage.jsx
+++ src/components/SurveyPage/SurveyPage.jsx
@@ -30,8 +30,42 @@ export default function SurveyPage() {
     return page === 20 ? 'Submit' : 'Next'
   }
 
+  const autofill = () => {
+    dispatch({
+      type: 'SET_SURVEY_ANSWERS',
+      payload: {
+        "1": -0.25,
+        "2": -0.5,
+        "3": -1,
+        "4": 0,
+        "5": -0.25,
+        "6": 0,
+        "7": -0.25,
+        "8": -0.25,
+        "9": -0.75,
+        "10": 0,
+        "11": 0,
+        "12": -0.25,
+        "13": 0.5,
+        "14": -0.25,
+        "15": -0.25,
+        "16": 0.5,
+        "17": 0.25,
+        "18": 0,
+        "19": 0,
+        "20": 0,
+        "21": "no",
+        "22": "no"
+    }
+  })
+    dispatch({ type: 'SURVEY/POST_DATA', payload: surveyData })
+  }
+
   return (
     <section id="survey-body">
+      <button
+        onClick={autofill}
+      >Click me to autofill & test</button>
       <SurveyQuestion page={page} />
       <SurveyOptions page={page} />
       <div className="survey-previous-next">
