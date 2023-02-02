683cbf590740156e4d1aa25706aa6f6d08f1559e
traceytreat <treatracey@gmail.com>
2023-02-02 09:34:49 -0600

    various css fixes

--- src/components/SurveyIntro/SurveyIntro.jsx
+++ src/components/SurveyIntro/SurveyIntro.jsx
@@ -1,12 +1,12 @@
 import './SurveyIntro.css';
 export default function SurveyIntro() {
   return (
-    <>
+    <section id="survey-header">
       <h2 id='survey-intro-welcome'>Welcome</h2>
       <section id="survey-intro">
         Please complete the following onboarding questions to create your
         personalized gaming experience.
       </section>
-    </>
+    </section>
   )
 }
