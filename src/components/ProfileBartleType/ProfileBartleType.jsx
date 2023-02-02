be7270ade0726e9debf5d1bfc7ea8f1dec5f11a6
Billy Salinas <wsalinas315@gmail.com>
2023-02-02 10:13:24 -0600

    ProfileBartleType logic finished, however this code will not work until it can get user genre scores from a reducer. Profile will not yet call ProfileBartleType for this reason

--- src/components/ProfileBartleType/ProfileBartleType.jsx
+++ src/components/ProfileBartleType/ProfileBartleType.jsx
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export default function ProfileBartleType() {
 
-  // local object
-  let bartleScores = {
-    killer: {score: 0, type: 'Killer'},
-    competitor: {score: 0, type: 'Competitor'},
-    collector: {score: 0, type: 'Collector'},
-    expert: {score: 0, type: 'Expert'},
-    detective: {score: 0, type: 'Detective'},
-    navigator: {score: 0, type: 'Navigator'},
-    rockstar: {score: 0, type: 'Rockstar'},
-    coach: {score: 0, type: 'Coach'}
-  };
+  // local arrays
+  let bartleScores = [
+    {score: 0, type: 'Killer'},
+    {score: 0, type: 'Competitor'},
+    {score: 0, type: 'Collector'},
+    {score: 0, type: 'Expert'},
+    {score: 0, type: 'Detective'},
+    {score: 0, type: 'Navigator'},
+    {score: 0, type: 'Rockstar'},
+    {score: 0, type: 'Coach'}
+  ];
+  let sortedBartles = [];
 
   // initialize variable from store
   const userGenres = useSelector(store => store.user_genres);
+
+  // Initialize local state
+  const [bartleType, setBartleType] = useState('');
 
   // calls calculatePlayerType function on page load
   useEffect(() => {
@@ -25,6 +29,7 @@ export default function ProfileBartleType() {
 
   // Calculate player type
   const calculatePlayerType = () => {
+    // add up bartle scores for each type
     bartleScores.killer.score = userGenres.shooter + userGenres.fighting + userGenres.action + userGenres.rpg;
     bartleScores.competitor.score = userGenres.sports + userGenres.racing + userGenres.arcade + userGenres.boardgames;
     bartleScores.collector.score = userGenres.platformer + userGenres.adventure + userGenres.rpg + userGenres.puzzle;
@@ -33,11 +38,16 @@ export default function ProfileBartleType() {
     bartleScores.navigator.score = userGenres.adventure + userGenres.simulation + userGenres.action + userGenres.indie;
     bartleScores.rockstar.score = userGenres.mmo + userGenres.shooter + userGenres.rpg + userGenres.adventure;
     bartleScores.coach.score = userGenres.family + userGenres.educational + userGenres.casual + userGenres.mmo;
+
+    // sort bartleScores array by score value
+    sortedBartles = bartleScores.sort((s1, s2) => (s1.score < s2.score) ? 1 : (s1.score > s2.score) ? -1 : 0);
+    // set bartleType
+    setBartleType(sortedBartles[0].type);
   }
 
   return (
   <div>
-    <h3>Player Type: {PlAyErTyPe}</h3>
+    <h3>Player Type: {bartleType}</h3>
   </div>
   )
 }
