21ebe1ff3d5074eaf602b907613de6e45126bfb4
Billy Salinas <wsalinas315@gmail.com>
2023-02-05 14:10:16 -0600

    added user scores SQL query

--- server/routes/rawg.router.js
+++ server/routes/rawg.router.js
@@ -65,29 +65,39 @@ router.get('/byGenre/:genre', async (req, res) => {
 
 router.get('/byGenre/', async (req, res) => {
 
+  // initialize empty array
   const searchQueries = [];
+
+  // current user ID
   const userID = req.user.id;
 
+  // userScores is a combination of genre and tag names & scores
+  const userScores = pool.query(`SELECT "genre_name" AS "name", "genre_score" AS "score" FROM "user_genres" WHERE "user_id" = $1
+                                UNION
+                                SELECT "tag_name" AS "name", "score" FROM "user_tags" WHERE "user_id" = $1;`, [userID]);
+  console.log('UserScores looks like:', userScores);
+
+  // ignoreList is a combination of game IDs for the current user's wishlist, played list and ignore list
   const ignoreList = pool.query(`SELECT "game_id" FROM "wishlist" WHERE "user_id" = $1
                                 UNION
                                 SELECT "game_id" FROM "ignorelist" WHERE "user_id" = $1
                                 UNION
-                                SELECT "game_id" FROM "played" WHERE "user_id" = $1;`);
+                                SELECT "game_id" FROM "played" WHERE "user_id" = $1;`, [userID]);
   console.log('IgnoreList looks like:', ignoreList);
 
   // get list of positively rated user genres, 
   // randomize list 
   // and only query RAWG for the first 3
   let userGenres = await pool.query(`SELECT "genre_name" FROM "user_genres" WHERE "user_id" = $1 AND genre_score > 0;`, [userID]);
-  console.log('userGenres looks like:',userGenres);
-  const genreList = await shuffleArray(userGenres).slice(0,3);
+  console.log('userGenres looks like:', userGenres);
+  const genreList = await shuffleArray(userGenres).slice(0, 3);
   console.log('genreList looks like:', genreList);
 
   // HELPER FUNCTIONS
   //* function to shuffle array for randomization and only return 3 genres
   const shuffleArray = (arr) => {
-    for (let i = arr.length -1; i > 0; i--){
-      const j = Math.floor(Math.random()*(i+1));
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
       [arr[i], arr[j]] = [arr[j], arr[i]];
     }
     return arr;
@@ -146,7 +156,7 @@ router.get('/byGenre/', async (req, res) => {
     // this function needs tweaking for comparing correct pieces of data
     taggedGames = taggedGames.filter(val => !ignoreList.includes(val));
 
-    
+
 
     res.send(taggedGames);
 
