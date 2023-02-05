48fb375813984e4191610f92acccc1137956208f
August McAllister <august.chm@gmail.com>
2023-02-05 12:26:14 -0600

    feat: updated database.sql with survey_questions table and insert values

--- database.sql
+++ database.sql
@@ -79,6 +79,19 @@ CREATE TABLE "glossary" (
   OIDS=FALSE
 );
 
+CREATE TABLE "survey_questions" (
+	"id" 			serial NOT NULL,
+	"question" 		varchar(255) NOT NULL,
+	"caption"		varchar(500),
+	"genres_left" 	varchar(40)[],
+	"genres_right"  varchar(40)[],
+	"tags_left" 	varchar(40)[],
+	"tags_right" 	varchar(40)[],
+	CONSTRAINT "survey_questions_pk" PRIMARY KEY ("id")
+) WITH (
+	OIDS=FALSE
+);
+
 ALTER TABLE "user_tags" ADD CONSTRAINT "user_tags_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
 ALTER TABLE "user_tags" ADD CONSTRAINT "user_tags_fk1" FOREIGN KEY ("tag_id") REFERENCES "tag"("id");
 ALTER TABLE "played" ADD CONSTRAINT "played_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
@@ -124,3 +137,148 @@ INSERT INTO "glossary" ("term")
 VALUES ('Action'),('Indie'),('Adventure'),('Strategy'),('Shooter'),('Casual'),('Simulation'),('Puzzle'),
 ('Arcade'),('Platformer'),('Racing'),('Massively Multiplayer'),('Sports'),('Fighting'),('Family'),('Board Games'),
 ('Educational'),('Card');
+
+
+-- Survey Questions, captions, and answers
+INSERT INTO "survey_questions"
+	("question", "caption", "genres_left", "genres_right", "tags_left", "tags_right")
+	VALUES
+	-- question 1:
+	('Would you rather control a single character/group of characters, or control a simulation?',
+	'In many games you often participate as a single character, or at most, a small team. In contrast, a simulation is a game where you participate in a series of events as an abstract actor, as if you were moving pieces in a virtual board game. Perhaps you''re giving orders to an army, directing the growth of a civilization, or sowing chaos in a small suburb.',
+	array['action', 'indie', 'adventure', 'rpg', 'shooter', 'platformer', 'massively-multiplayer', 'fighting'],
+	array['strategy', 'simulation', 'puzzle', 'sports', 'board-games', 'card'],
+	array['rpg'],
+	array['base-building', 'city-builder', 'clicker', 'colony-sim', 'card-game', 'deck-building', 'tower-defense']),
+
+	-- question 2:
+	('Are you mainly looking for new adventures for yourself, or do you want to bring some friends along?',
+	'There are plenty of offerings for most group sizes. Many games are exclusively single-player or exclusively multiplayer, but quite a few games support both modes!',
+	array[]::text[],
+	array['massively-multiplayer', 'fighting'],
+	array['singleplayer', 'single-player', 'solo', 'solo-rpg'],
+	array['multiplayer', 'two-players', 'two-player', 'battle-royale', 'pvp', 'local-co-op', 'local-multiplayer', 'local-pvp', 'split-screen']
+	),
+	
+	-- question 3:
+	('If and when playing with friends, would you like to cooperate toward a goal, or compete against each other?',
+	'Cooperative games and game modes are referred to as co-op, and sometimes as PvE (Player versus Environment). Competitive game modes are often referred to as PvP (Player versus Player).',
+	array[]::text[],
+	array[]::text[],
+	array['co-op', 'co-op-campaign', 'pve', 'local-co-op', 'mmo', 'mmorpg', 'online-co-op', 'online-multiplayer'],
+	array['pvp', 'competitive', 'battle-royale', 'local-pvp', 'moba', 'online-pvp']
+	),
+	
+	-- question 4:
+	('Would you rather play something easy, or experience a challenge?',
+	'Keep in mind that many games have adjustable difficulty settings; don''t be ashamed to adjust the difficulty to your liking.',
+	array['casual', 'family'],
+	array['strategy', 'rpg', 'puzzle'],
+	array['relaxing', 'family-friendly', 'walking-simulator', 'non-violent', 'party-game', 'feel-good'],
+	array['souls-like', 'unforgiving', 'difficult', 'perma-death', 'roguelike', 'roguelite', 'action-roguelike']),
+	
+	-- question 5:
+	('Do you prefer short video games over long ones?',
+	'A game is generally considered short if it can be completed under 8-10 hours. Long games can take anywhere from 30 hours to hundreds to see all of their content.',
+	array['casual', 'puzzle', 'board-games', 'card'],
+	array['adventure', 'rpg', 'massively-multiplayer'],
+	array['short'],
+	array['jrpg']),
+	
+	-- question 6:
+	('Would you like to play a game with a stylized, cartoony, or animated graphical style?',
+	'',
+	array[]::text[],
+	array[]::text[],
+	array['anime', 'manga', 'cartoon', 'stylized', 'comic-book', 'colorful', 'hand-drawn', '8-bit', 'pixel-graphics', 'retro', 'demake', 'low-poly', 'old-school'],
+	array[]::text[]),
+	
+	-- question 7:
+	('Which sounds more appealing to you: a fantasy/sci-fi setting, where advanced technology or mythical creatures could play a role; or a setting in a grounded, realistic world much like our own?',
+	'',
+	array['adventure', 'rpg', 'massively-multiplayer'],
+	array[]::text[],
+	array['zombies', 'western', 'vampire', 'supernatural', 'steampunk', 'space', 'space-sim', 'sci-fi', 'fantasy', 'futuristic', 'monsters'],
+	array[]::text[]),
+	
+	-- question 8:
+	('Would you like to feel fully immersed and involved in the game world, or would you rather observe characters/events from a distance?',
+	'"Immersion" is one of the most difficult game terms to define. In short, some games strive to really place you in the main character''s shoes, and to create a game world that feels wondrous and alive. Others let you take more of a bird''s eye view of events, or place some distance between the character and the player (sometimes literally, as in your field of view, and sometimes in how you relate to the character).',
+	array[]::text[],
+	array[]::text[],
+	array['open-world', 'first-person', 'fps', 'atmospheric', 'sandbox', 'silent-protagonist'],
+	array['god-game', 'rts', 'real-time-tactics', 'moba', 'base-building', 'city-builder', 'isometric', 'top-down']),
+	
+	-- question 9:
+	('Would you prefer an exciting, fast-paced game that rewards movement and reaction time, or a slower, more thoughtful game that rewards planning, tactics and strategy?',
+	'Fast-paced games would include first-person shooters, platformers, and racing games; slower-paced games would include strategy games, and games with turn-based combat (where you take turns with the computer-controlled enemies).',
+	array['action', 'platformer', 'fighting', 'racing', 'shooter', 'arcade', 'sports'],
+	array['strategy','puzzle','simulation','card','board-games'],
+	array['action-rpg', 'bullet-hell', 'fast-paced', 'fps', 'precision-platformer', 'quick-time-events', 'music', 'rhythm', 'shoot-em-up', 'beat-em-up', 'twin-stick-shooter','horror', 'survival', 'action-adventure'],
+	array['crpg', 'action-rts', 'rts', 'resource-management', 'turn-based']),
+	
+	-- question 10:
+	
+	('Which sounds more appealing: a game where you repeat activities to gain better scores, gear, and abilities; or a game with a structure and a definitive ending?',
+	'Many games follow a story with an explicit conclusion, or have a certain number of levels to clear. But other games don''t have a set ending point; you can continue playing and repeating activities until you''ve mastered them, or until you''ve had your fill.',
+	array['arcade', 'massively-multiplayer', 'simulation', 'card', 'board-games'],
+	array['indie', 'adventure', 'action', 'rpg'],
+	array['mmo', 'score-attack', 'roguelike', 'roguelite', 'roguevania'],
+	array['linear', 'multiple-endings']),
+	
+	-- question 11:
+	('Would you rather play a game where you feel powerful and have many abilities, or a game where you are weak and disempowered, and you have to beat the odds?',
+	'Some players find a lot of enjoyment in satisfying combat, fluid movement, and playing a character that has a fighting chance against anything they encounter. Others prefer to have the deck stacked against them by playing a weak character, or playing against overwhelming odds, and persisting until they overcome them.',
+	array['action', 'rpg', 'arcade', 'fighting'],
+	array[]::text[],
+	array['beat-em-up', 'superhero', 'superpowers', 'spectacle-fighter', 'action-adventure'],
+	array['horror', 'survival', 'survival-horror', 'difficult', 'lovecraftian', 'spooky', 'stealth', 'unforgiving']),
+	
+	-- question 12:
+	('Would you feel more accomplished from demonstrating your practical skills (craftsmanship, puzzle solving, etc), or your heroic skills (magic, combat abilities, etc)?',
+	'It''s not all valiant knights and space wizards! There are games that let you manage a farm, run a business, power wash dirty objects, drive a delivery truck, and much more.',
+	array['simulation', 'educational', 'puzzle'],
+	array['action', 'adventure', 'rpg', 'shooter', 'strategy', 'arcade'],
+	array['argiculture', 'farming', 'gambling', 'management', 'resource-management', 'fishing', 'flight', 'driving', 'gardening', 'hunting', 'crafting', 'logic', 'puzzle-platformer'],
+	array['rpg', 'magic', 'fantasy', 'loot', 'dungeon-crawler']),
+	
+	-- question 13:
+	('Do you find the most enjoyment in feeling unrestricted and free to explore, or in having a sense of direction?',
+	'Open world games allow the player access to a wide-open world, with little to no hand-holding or mandatory waypoints. These games can be very immersive, and allow the player a lot of freedom, but can sometimes lack the focus and the hand-crafted feeling of games that are divided into levels or linear experiences.',
+	array['adventure', 'rpg', 'massively-multiplayer'],
+	array['action', 'shooter', 'platformer', 'arcade', 'racing'],
+	array['open-world', 'immersive-sim', 'atmospheric', 'nonlinear', 'metroidvania', 'exploration'],
+	array['linear']),
+	
+	-- question 14:
+	('Would you like to play a game with a strong focus on story, character, and narrative?',
+	'Video games have grown immensely over the past thirty years, and now rival books, movies, theater, and television in their ability to tell a compelling narrative. If you love a good story or endearing characters, there is something out there for you.',
+	array['adventure', 'rpg'],
+	array[]::text[],
+	array['emotional', 'epic', 'story', 'story-rich', 'meaningful-choices', 'choices-matter', 'choose-your-own-adventure', 'dating-sim', 'walking-simulator', 'lore-rich', 'multiple-endings', 'mystery', 'narration', 'narrative', 'point-click', 'story', 'story-rich'],
+	array[]::text[]),
+	
+	-- question 15:
+	('Would you like to play a game with an intense, dark, or otherwise serious tone?',
+	'The narrative tone of games can vary immensely, and the subject matter of some games can skew distinctly macabre. These games can be very compelling and thought-provoking, but it''s ok if you''d like to focus on having a good time when you play.',
+	array[]::text[],
+	array[]::text[],
+	array['emotional', 'dark', 'dark-comedy', 'crime', 'villain-protagonist', 'drama', 'dystopian', 'noir', 'horror'],
+	array[]::text[]),
+	
+	-- question 16:
+	('Would you prefer to avoid games with gory or very violent content?',
+	'',
+	array[]::text[],
+	array[]::text[],
+	array['family-friendly'],
+	array['blood', 'gore', 'violent', 'horror']),
+	
+	-- question 17:
+	('Would you prefer to avoid games with sexual or erotic content?',
+	'',
+	array[]::text[],
+	array[]::text[],
+	array[]::text[],
+	array['nsfw', 'nudity', 'erotic', 'sexual-content', 'dating-sim'])
+;
