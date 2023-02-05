45264afa3d425f9887fd445d978ca64f1fb48220
August McAllister <august.chm@gmail.com>
2023-02-05 17:48:27 -0600

    feat: wrote /api/user/scores query to get user scores from DB

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const {
-	rejectUnauthenticated,
-} = require('../modules/authentication-middleware');
-const encryptLib = require('../modules/encryption');
-const pool = require('../modules/pool');
-const userStrategy = require('../strategies/user.strategy');
-
 const router = express.Router();
+const encryptLib = require('../modules/encryption');
+const userStrategy = require('../strategies/user.strategy');
+const pool = require('../modules/pool');
 
-// Handles Ajax request for user information if user is authenticated
+const { default: axios } = require('axios');
+
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+
+// ================================
+// USER AUTH ROUTES
+
 router.get('/', rejectUnauthenticated, (req, res) => {
-	// Send back user object from the session (previously queried from the database)
 	res.send(req.user);
 });
 
-// Handles POST request with new user data
-// The only thing different from this and every other post we've seen
-// is that the password gets encrypted before being inserted
 router.post('/register', (req, res, next) => {
 	const username = req.body.username;
 	const password = encryptLib.encryptPassword(req.body.password);
@@ -33,19 +31,34 @@ router.post('/register', (req, res, next) => {
 		});
 });
 
-// Handles login form authenticate/login POST
-// userStrategy.authenticate('local') is middleware that we run on this route
-// this middleware will run our POST if successful
-// this middleware will send a 404 if not successful
 router.post('/login', userStrategy.authenticate('local'), (req, res) => {
 	res.sendStatus(200);
 });
 
-// clear all server session information about this user
 router.post('/logout', (req, res) => {
-	// Use passport's built-in method to log out the user
 	req.logout();
 	res.sendStatus(200);
 });
+
+// ================================
+// USER INFO ROUTES
+
+// GET user scores
+router.get('/scores', rejectUnauthenticated, async (req, res) => {
+
+  try {
+    const genreQuery = 'SELECT * FROM user_genres WHERE user_id = $1'
+    const tagQuery = 'SELECT * FROM user_tags WHERE user_id = $1'
+
+    const userGenreScores = await pool.query(genreQuery, [req.user.id])
+    const userTagScores = await pool.query(tagQuery, [req.user.id])
+
+    res.send([userGenreScores, userTagScores])
+  } catch (err) {
+    console.log('Failed to get user scores', err);
+    res.sendStatus(500)
+  }
+
+})
 
 module.exports = router;
