862cd64732039deade54a6aebae98585ddd078f8
Billy Salinas <wsalinas315@gmail.com>
2023-02-05 13:19:20 -0600

    rawg router updates with last commit, hadn't staged yet

--- server/routes/rawg.router.js
+++ server/routes/rawg.router.js
@@ -61,6 +61,88 @@ router.get('/byGenre/:genre', async (req, res) => {
 })
 
 // ==========================================================================================
+// GET BY GENRE - TEST FOR RECOMMENDATIONS
+
+router.get('/byGenre/', async (req, res) => {
+
+  const searchQueries = [];
+  const userID = req.user.id;
+
+  // get list of positively rated user genres, 
+  // randomize list 
+  // and only query RAWG for the first 3
+  let userGenres = await pool.query(`SELECT "genre_name" FROM "user_genres" WHERE "user_id" = $1 AND genre_score > 0;`, [userID]);
+  const genreList = await shuffleArray(userGenres).slice(0,3);
+
+  // HELPER FUNCTIONS
+  //* function to shuffle array for randomization and only return 3 genres
+  const shuffleArray = (arr) => {
+    for (let i = arr.length -1; i > 0; i--){
+      const j = Math.floor(Math.random()*(i+1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+
+  //* function to filter out duplicate results
+  const dupeFilter = (obj, idx, arr) => {
+    return idx === arr.findIndex((game) => {
+      return game.name === obj.name && game.id === obj.id
+    })
+  }
+
+  //* filters out non-english tags, isolates the tag name, and sorts alphabetically
+  const tagFilter = (game) => {
+    const gameTags =
+      game.tags
+        .filter(tag => tag.language === "eng")
+        .map(tag => tag.name)
+        .sort();
+
+    //* returns game object, with tags filtered
+    return {
+      ...game,
+      tags: gameTags.map(tag => tag.toLowerCase())
+    }
+  }
+
+  //* sorts games in order of most matched tags to fewest
+  const sortByTagRelevance = (a, b) => {
+    return b.tags.filter(tag => userTags.includes(tag.toLowerCase())).length -
+      a.tags.filter(tag => userTags.includes(tag.toLowerCase())).length
+  }
+
+  console.log('searching for genre', genre, '...');
+  try {
+    for (let genre of genreList) {
+      searchQueries.push(axios.get(`https://api.rawg.io/api/games?genres=${genre.toLowerCase()}&${keyUrl}&page_size=40`,
+        { validateStatus: (status) => status < 500 })) // prevents request from throwing error if it returns a 404
+      searchQueries.push(axios.get(`https://api.rawg.io/api/games?genres=${genre.toLowerCase()}&${keyUrl}&page_size=40&page=2`,
+        { validateStatus: (status) => status < 500 }))
+      searchQueries.push(axios.get(`https://api.rawg.io/api/games?genres=${genre.toLowerCase()}&${keyUrl}&page_size=40&page=3`,
+        { validateStatus: (status) => status < 500 }))
+    }
+    const taggedGameObjects = await Promise.all(searchQueries);
+
+    const taggedGames =
+      taggedGameObjects
+        .filter(obj => obj.status < 300) // filters out any queries that returned a 404
+        .map(obj => obj.data.results) // isolates the actual API results
+        .flat() // flattens the results into a single one-dimensional array
+        .filter(dupeFilter)
+        .map(tagFilter)
+        .sort(sortByTagRelevance);
+
+    res.send(taggedGames);
+
+
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
+})
+
+// ==========================================================================================
 // GET BY RELEVANT TAGS
 
 router.get('/byTags', async (req, res) => {
@@ -71,7 +153,7 @@ router.get('/byTags', async (req, res) => {
   // const userID = req.user.id;
   // const userTags = await pool.query(`SELECT "tag_name" FROM "user_tags" WHERE "user_id" = $1 AND score > 0;`, [userID]);
 
-  const searchQueries = []
+  const searchQueries = [];
 
   // HELPER FUNCTIONS
   //* function to filter out duplicate results
@@ -107,11 +189,11 @@ router.get('/byTags', async (req, res) => {
 
     for (let tag of userTags) {
       searchQueries.push(axios.get(`https://api.rawg.io/api/games?${keyUrl}&tags=${tag.toLowerCase()}&page_size=40`,
-        { validateStatus: (status) => status < 500 } )) // prevents request from throwing error if it returns a 404
+        { validateStatus: (status) => status < 500 })) // prevents request from throwing error if it returns a 404
       searchQueries.push(axios.get(`https://api.rawg.io/api/games?${keyUrl}&tags=${tag.toLowerCase()}&page_size=40&page=2`,
-        { validateStatus: (status) => status < 500 } ))
+        { validateStatus: (status) => status < 500 }))
       searchQueries.push(axios.get(`https://api.rawg.io/api/games?${keyUrl}&tags=${tag.toLowerCase()}&page_size=40&page=3`,
-        { validateStatus: (status) => status < 500 } ))
+        { validateStatus: (status) => status < 500 }))
     }
 
     const taggedGameObjects = await Promise.all(searchQueries)
@@ -124,7 +206,7 @@ router.get('/byTags', async (req, res) => {
         .filter(dupeFilter)
         .map(tagFilter)
         .sort(sortByTagRelevance);
-    
+
     res.send(taggedGames)
 
   } catch (err) {
