bdd629f63abb722d2f91810ff36796a6c19e5860
August McAllister <august.chm@gmail.com>
2023-02-06 15:16:22 -0600

    feat: added RecommendedList component to render recommendations to DOM

---
+++ src/components/RecommendedList/RecommendedList.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useRecommendations } from "../../hooks/storeHooks";
+
+export default function RecommendedList() {
+  const dispatch = useDispatch();
+  const recommendations = useRecommendations();
+
+  useEffect(() => {
+    dispatch({ type: "RAWG/FETCH_RECOMMENDATIONS" });
+  }, []);
+
+  const ListItem = ({ gameData }) => {
+    return (
+      <div className="recommended">
+        <a href="/#/">
+          <div className="card">
+            <h1>{gameData.name}</h1>
+          </div>
+        </a>
+      </div>
+    );
+  };
+
+  return (
+    <>
+    {/* {JSON.stringify(recommendations)} */}
+      {recommendations
+        .map((game) => {
+          return <ListItem gameData={game.gameData} />;
+        })
+        .sort((a, b) => a.gameScore - b.gameScore)}
+    </>
+  );
+}
