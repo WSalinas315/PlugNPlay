9f1ca542a4cd121fa455ee718b8ee36773e64578
August McAllister <august.chm@gmail.com>
2023-02-07 11:07:53 -0600

    feat: added various details to GameItem view; added footer to allow for scrollable content to be shown

--- src/components/GameItem/GameItem.jsx
+++ src/components/GameItem/GameItem.jsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+
+import { formatDate } from "../../helpers/dates";
 import { useGameByID } from "../../hooks/storeHooks";
-import { capitalizeFirst } from "../../helpers/words"
+import { capitalizeFirst } from "../../helpers/words";
 import Loading from "../Loading/Loading";
 
 export default function () {
@@ -11,23 +13,77 @@ export default function () {
   const game = useGameByID();
 
   useEffect(() => {
+    window.scrollTo(0, 0);
     dispatch({ type: "RAWG/FETCH_CURRENT_GAME", payload: id });
   }, []);
+
+  const YoutubeLink = () => {
+    return (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href={`https://www.youtube.com/results?search_query=${game.slug.replaceAll(
+          "-",
+          "+"
+        )}+game+trailer`}
+      >
+        Check out some trailers
+      </a>
+    );
+  };
+
+  const MetacriticLink = () => {
+    return (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href={game.metacritic_url}
+      >
+        Rated {game.metacritic} on Metacritic
+      </a>
+    )
+  }
+
+  const GenreUL = () => {
+    return (
+      <ul>
+        {game.genres.map((genre) => (
+          <li>{genre.name}</li>
+        ))}
+      </ul>
+    );
+  };
 
   return game.name ? (
     <>
       <div>
         <h1>{game.name}</h1>
+        <p>
+          <MetacriticLink />
+        </p>
+        <p>
+          <YoutubeLink />
+        </p>
         <p>Genres:</p>
-        <ul>
-          {game.genres?.map(genre => <li>{genre.name}</li>)}
-        </ul>
+        <GenreUL />
         <p>Tags:</p>
         <ul>
-          {game.tags?.map(tag => <li>{capitalizeFirst(tag.name)}</li>)}
+          {game.tags.map((tag) => (
+            <li>{capitalizeFirst(tag.name)}</li>
+          ))}
         </ul>
-          {game.description_raw}
+        <p>Available for:</p>
+        <ul>
+          {game.platforms.map(({ platform }) => (
+            <li>{platform.name}</li>
+          ))}
+        </ul>
+        <p>Released {formatDate(game.released)}</p>
+        <p>{game.description_raw}</p>
+        <img src={game.background_image} />
       </div>
     </>
-  ) : <Loading />
+  ) : (
+    <Loading />
+  );
 }
