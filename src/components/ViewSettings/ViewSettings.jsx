190b641309cdbe742a16fdb930fcfebbfa93f899
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2023-02-10 13:27:35 -0600

    settings page

--- src/components/ViewSettings/ViewSettings.jsx
+++ src/components/ViewSettings/ViewSettings.jsx
@@ -3,20 +3,35 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 
 function ViewSettings (){
-let newItem= "test"
+
+  const [ newItem, setNewItem ] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // write reducer, saga, and PUT route to db
+  }
 
   return (
     
     <div>
     <h1>Settings</h1>
-    (edit) 
-    <form onSubmit={(event) => handleSubmit(event)}>
+    Change Profile Picture 
+    <form onSubmit={handleSubmit}>
         <input
             className="input"
             value={newItem}
             onChange={(e) => setNewItem(e.target.value)}
             placeholder="Edit "
          />
+    <div>
+      <img src="image-url" alt="image" />
+    </div>
+         <div align="center">
+            <button type='submit'>
+              Edit
+            </button>
+          </div>
     </form>
     </div>
  )
