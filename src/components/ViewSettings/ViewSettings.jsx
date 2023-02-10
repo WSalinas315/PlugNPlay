70b20e80c19fcfccda7649428251c65bf43f8d0d
August McAllister <august.chm@gmail.com>
2023-02-10 13:50:30 -0600

    feat: applied MUI theme & styles to user settings page

--- src/components/ViewSettings/ViewSettings.jsx
+++ src/components/ViewSettings/ViewSettings.jsx
@@ -1,41 +1,41 @@
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
-function ViewSettings (){
+import { Card, Button, FormControl, TextField } from "@mui/material";
 
-  const [ newItem, setNewItem ] = useState('');
+import Heading1 from "../Headings/Heading1";
+import Heading3 from "../Headings/Heading3";
+
+export default function ViewSettings() {
+  const [newItem, setNewItem] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     // write reducer, saga, and PUT route to db
-  }
+  };
 
   return (
-    
-    <div>
-    <h1>Settings</h1>
-    Change Profile Picture 
-    <form onSubmit={handleSubmit}>
-        <input
-            className="input"
+    <Card>
+      <Heading1>Settings</Heading1>
+      <form onSubmit={handleSubmit}>
+          <Heading3 sx={{ mt: '1rem', mb: '0.5rem' }}>Edit Profile Picture</Heading3>
+          <TextField
+            sx={{ width: '100%', mb: '2rem' }}
+            className="grid-right"
             value={newItem}
             onChange={(e) => setNewItem(e.target.value)}
-            placeholder="Edit "
-         />
-    <div>
-      <img src="image-url" alt="image" />
-    </div>
-         <div align="center">
-            <button type='submit'>
-              Edit
-            </button>
-          </div>
-    </form>
-    </div>
- )
+            label="Profile Picture URL"
+          />
+        <div align="center">
+          <Button
+            sx={{ width: '70%', m: '0.3rem' }}
+            variant="outlined" type="submit">
+            Save Changes
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
 }
-
-
-export default ViewSettings
