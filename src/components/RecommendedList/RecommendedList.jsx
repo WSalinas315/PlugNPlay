b83b04e84d7322a732ed6f6b909be3a42ea46edb
August McAllister <august.chm@gmail.com>
2023-02-11 13:40:59 -0600

    feat: further styling; began working on buttons for swipe box

--- src/components/RecommendedList/RecommendedList.jsx
+++ src/components/RecommendedList/RecommendedList.jsx
@@ -1,10 +1,9 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useRecommendations } from "../../hooks/storeHooks";
-import { Link } from "react-router-dom";
 import Loading from "../Loading/Loading";
 import SwipeBox from "../SwipeBox/SwipeBox";
-import { Card } from "@material-ui/core";
+import { Card } from "@mui/material";
 import Heading1 from "../Headings/Heading1";
 import ParagraphText from "../ParagraphText/ParagraphText";
 
@@ -12,48 +11,26 @@ export default function RecommendedList() {
   const dispatch = useDispatch();
   const recommendations = useRecommendations();
 
+  const clearGameData = () => dispatch({ type: "GAME/CLEAR_CURRENT" });
+
   const checkForRecs = (refresh = false) => {
-    dispatch({ type: "GAME/CLEAR_CURRENT" });
+    clearGameData();
     if (refresh || recommendations.length === 0) {
-      dispatch({ type: "RAWG/FETCH_RECOMMENDATIONS" })
+      // dispatch({ type: "RAWG/FETCH_RECOMMENDATIONS" })
     } else return;
-  }
-
-  const clearGameData = () => dispatch({ type: 'GAME/CLEAR_CURRENT' })
-
-  useEffect(checkForRecs, []);
-  
-  const ListItem = ({ gameData }) => {
-    return (
-      <div className="recommended">
-        <Link to={`/games/${gameData.id}`} onClick={clearGameData} >
-          <div className="card">
-            <h2>{gameData.name}</h2>
-            <p>{gameData.genres.map((genre) => genre.name + " ")}</p>
-          </div>
-        </Link>
-      </div>
-    );
   };
 
-  const Bold = ({children}) => {
-    return <span style={{ fontWeight: 800 }}>{children}</span>
-  }
+  useEffect(checkForRecs, []);
 
   return recommendations.length > 0 ? (
-      <Card sx={{ textAlign: 'center', px: '10px' }}>
-        <Heading1>Your Recommendations</Heading1>
+    <div className="center-on-page">
+      <Card sx={{ border: "1px solid", borderColor: "#dddddd", py: "1.5rem" }}>
+        <Heading1 sx={{ textAlign: "center" }}>Recommended for You</Heading1>
         <SwipeBox games={recommendations} />
-        <ParagraphText>
-          Swipe <Bold>right</Bold> to add a game to your wishlist.
-        </ParagraphText>
-        <ParagraphText>
-          Swipe <Bold>up</Bold> to view more details.
-        </ParagraphText>
-        <ParagraphText>
-          Swipe <Bold>left</Bold> to skip a game.
-        </ParagraphText>
+
+        {JSON.stringify(recommendations[0].gameData.id)}
       </Card>
+    </div>
   ) : (
     <Loading />
   );
