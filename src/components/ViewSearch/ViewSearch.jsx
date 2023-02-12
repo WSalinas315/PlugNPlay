5fc95d030adcf9768df18542a7f4ce11122bb025
August McAllister <august.chm@gmail.com>
2023-02-12 17:56:04 -0600

    feat: re-styled view search component to use established styles

--- src/components/ViewSearch/ViewSearch.jsx
+++ src/components/ViewSearch/ViewSearch.jsx
@@ -1,13 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { Button, FormControl, TextField, Typography } from '@mui/material';
-import Select from '@mui/material/Select';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import {
+  Button,
+  Card,
+  FormControl,
+  TextField,
+} from "@mui/material";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Heading1 from "../Headings/Heading1";
+import Heading3 from "../Headings/Heading3";
 
 export default function ViewSearch() {
-
   // initialize dispatch
   const dispatch = useDispatch();
 
@@ -16,85 +22,102 @@ export default function ViewSearch() {
 
   // fetches subtypes from database
   useEffect(() => {
-    dispatch({ type: 'RAWG/FETCH_GENRE_LIST' });
+    dispatch({ type: "RAWG/FETCH_GENRE_LIST" });
   }, []);
 
   // Initialize local states for search criteria
-  const [gameTitle, setGameTitle] = useState('');
-  const [gameGenre, setGameGenre] = useState('');
+  const [gameTitle, setGameTitle] = useState("");
+  const [gameGenre, setGameGenre] = useState("");
 
   // initialize variables from store
-  const genreList = useSelector(store => store.games.genreList.results);
+  const genreList = useSelector((store) => store.games.genreList.results);
 
   const searchByName = () => {
     // Clear current search results
-    dispatch({ type: 'GAME/CLEAR_SEARCH_RESULTS' });
+    dispatch({ type: "GAME/CLEAR_SEARCH_RESULTS" });
     // Query RAWG for titles using current input
-    dispatch({ type: 'RAWG/SEARCH_BY_NAME', payload: gameTitle });
+    dispatch({ type: "RAWG/SEARCH_BY_NAME", payload: gameTitle });
     history.push(`/searchresults/${gameTitle}`);
-  }
+  };
 
   const searchByGenre = () => {
     // Clear current search results
-    dispatch({ type: 'GAME/CLEAR_SEARCH_RESULTS' });
+    dispatch({ type: "GAME/CLEAR_SEARCH_RESULTS" });
     // Query RAWG by Genre
-    dispatch({ type: 'RAWG/SEARCH_BY_GENRE', payload: gameGenre });
+    dispatch({ type: "RAWG/SEARCH_BY_GENRE", payload: gameGenre });
     history.push(`/searchresults/${gameGenre}`);
-  }
+  };
 
   return (
-    <>
-    {/* Page Label */}
-    <Typography variant="h4" sx={{ textAlign: "center", marginBottom: "15px", fontWeight: "bold" }}>Search Games</Typography>
+    <div>
+      <Card sx={{ border: "1px solid #dddddd" }}>
+        {/* Page Label */}
+        <Heading1 sx={{ textAlign: "center", my: "1rem" }}>
+          Search Games
+        </Heading1>
 
-      {/* Search By Name */}
-      <Typography variant="h6" sx={{ textAlign: "center", fontWeight: "bold" }}>Search By Game Title</Typography>
-      <FormControl fullWidth>
-        <TextField
-          required
-          value={gameTitle}
-          label="Title"
-          variant="outlined"
-          onChange={(event) => setGameTitle(event.target.value)}
-        />
-      </FormControl>
+        {/* Search By Name */}
+        <Heading3 sx={{ ml: "20px", mt: '1.2rem' }}>By Game Title</Heading3>
+        <div className="two-column-grid">
+          <div className="grid-left">
+            <FormControl fullWidth>
+              <TextField
+                required
+                value={gameTitle}
+                label="Title"
+                variant="outlined"
+                onChange={(event) => setGameTitle(event.target.value)}
+              />
+            </FormControl>
+          </div>
 
-      {/* Renders a disabled button if no text is in the search by game title input field
-          Renders a clickable button otherwise */}
-      {gameTitle
-        ?
-        <Button variant="outlined" onClick={() => searchByName()} sx={{ padding: '10px'}}>Search</Button>
-        :
-        <Button variant="outlined" disabled sx={{ padding: '10px'}}>Search</Button>
-      }
-      <br />
-      <br />
+          <div className="grid-right">
+            <Button
+              variant="outlined"
+              disabled={!gameTitle}
+              onClick={() => searchByName()}
+              sx={{ p: "10px" }}
+            >
+              Search
+            </Button>
+          </div>
+        </div>
 
-      {/* Search By Genre */}
-      <Typography variant="h6" sx={{ textAlign: "center", fontWeight: "bold" }}>Search By Genre</Typography>
-      <FormControl fullWidth>
-        <InputLabel id="genre">Genre</InputLabel>
-        <Select
-          required
-          id="genre"
-          label="Genre"
-          value={gameGenre}
-          onChange={(event) => setGameGenre(event.target.value)}
-        >
-          {genreList?.map((genre, i) => {
-            return(<MenuItem key={i} value={genre.slug}>{genre.name}</MenuItem>)
-          })}
-        </Select>
-      </FormControl>
-
-      {/* Renders a disabled button if no text is in the search by game title input field
-          Renders a clickable button otherwise */}
-      {gameGenre
-        ?
-        <Button variant="outlined" onClick={() => searchByGenre()} sx={{ padding: '10px'}}>Search</Button>
-        :
-        <Button variant="outlined" disabled sx={{ padding: '10px'}}>Search</Button>
-      }
-    </>
+        {/* Search By Genre */}
+        <Heading3 sx={{ ml: "20px", mt: "1.2rem" }}>By Genre</Heading3>
+        <div className="two-column-grid">
+          <div className="grid-left">
+            <FormControl fullWidth>
+              <InputLabel id="genre">Genre</InputLabel>
+              <Select
+                required
+                id="genre"
+                label="Genre"
+                value={gameGenre}
+                onChange={(event) => setGameGenre(event.target.value)}
+              >
+                {genreList?.map((genre, i) => {
+                  return (
+                    <MenuItem key={i} value={genre.slug}>
+                      {genre.name}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+          </div>
+          <div className="grid-right">
+            <Button
+              variant="outlined"
+              disabled={!gameGenre}
+              onClick={() => searchByGenre()}
+              sx={{ padding: "10px" }}
+            >
+              Search
+            </Button>
+          </div>
+        </div>
+      </Card>
+    </div>
   );
 }
