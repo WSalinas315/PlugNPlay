556086b8260cd82d57720976a17d98374fd0f1d3
August McAllister <august.chm@gmail.com>
2023-02-12 16:09:05 -0600

    feat: adjusted heading components to use sx prop correctly

--- src/components/Headings/Heading1.jsx
+++ src/components/Headings/Heading1.jsx
@@ -2,10 +2,10 @@ import { Box, Typography } from "@mui/material"
 
 export default function Heading1(props) {
 
-  const { children, ...rest } = props;
+  const { children, sx, ...rest } = props;
 
   return (
-    <Box sx={{ my: '0.6rem' }} {...rest} >
+    <Box sx={{ my: '0.6rem', ...sx }} >
       <Typography variant="h1">
         {children}
       </Typography>
--- src/components/Headings/Heading3.jsx
+++ src/components/Headings/Heading3.jsx
@@ -2,10 +2,10 @@ import { Box, Typography } from "@mui/material"
 
 export default function Heading3(props) {
 
-  const { children, ...rest } = props;
+  const { children, sx, ...rest } = props;
 
   return (
-    <Box sx={{ my: '0.6rem' }} {...rest} >
+    <Box sx={{ my: '0.6rem', ...sx }} >
       <Typography variant="h3">
         {children}
       </Typography>
