abccaf09c654c9949d6150514b2d817a05ebd312
August McAllister <august.chm@gmail.com>
2023-02-12 18:36:43 -0600

    feat: refactored and re-styled survey page to use established styles

--- src/components/SurveyPage/SurveyPage.jsx
+++ src/components/SurveyPage/SurveyPage.jsx
@@ -1,149 +1,192 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useSurveyData } from '../../hooks/storeHooks'
-import { useEffect } from 'react'
-import SurveyOptions from '../SurveyOptions/SurveyOptions'
-import SurveyNextButton from '../SurveyNextButton/SurveyNextButton'
-import SurveyPrevButton from '../SurveyPrevButton/SurveyPrevButton'
-import SurveyIntro from '../SurveyIntro/SurveyIntro'
-import './SurveyPage.css'
-import { useHistory, useParams } from 'react-router-dom'
-import Dialog from '@mui/material/Dialog'
-import DialogContent from '@mui/material/DialogContent'
-import DialogContentText from '@mui/material/DialogContentText'
-import DialogTitle from '@mui/material/DialogTitle'
-import HelpIcon from '@mui/icons-material/Help';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+
+import { useSurveyData } from "../../hooks/storeHooks";
+
+import SurveyOptions from "../SurveyOptions/SurveyOptions";
+import SurveyIntro from "../SurveyIntro/SurveyIntro";
+
+import "./SurveyPage.css";
+
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Button } from "@mui/material";
+import HelpIcon from "@mui/icons-material/Help";
+
+import Heading3 from "../Headings/Heading3";
 
 export default function SurveyPage() {
-  const surveyData = useSurveyData()
-  const dispatch = useDispatch()
-  console.log(surveyData)
-  const { id } = useParams()
-  const history = useHistory()
-  const surveyQuestion = useSelector((store) => store.survey.surveyQuestions)
-  const [open, setOpen] = useState(false)
+  const surveyData = useSurveyData();
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const history = useHistory();
+  const surveyQuestion = useSelector((store) => store.survey.surveyQuestions);
+  const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
-    setOpen(true)
-  }
+    setOpen(true);
+  };
 
   const handleClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
 
   useEffect(() => {
-    dispatch({ type: 'SURVEY/FETCH' })
-  }, [])
+    dispatch({ type: "SURVEY/FETCH" });
+  }, []);
 
   const nextPage = () => {
-    console.log('in nextpage')
+    console.log("in nextpage");
     if (id < 17) {
-      history.push('/survey/' + (Number(id) + 1))
+      history.push("/survey/" + (Number(id) + 1));
     } else {
-      dispatch({ type: 'SURVEY/POST_DATA', payload: surveyData })
-      history.push('/user')
+      dispatch({ type: "SURVEY/POST_DATA", payload: surveyData });
+      history.push("/user");
     }
-  }
+  };
 
   const prevPage = () => {
-    history.push('/survey/' + (Number(id) - 1))
-  }
+    history.push("/survey/" + (Number(id) - 1));
+  };
 
-  const nextBtnText = () => {
-    return Number(id) === 17 ? 'Submit' : 'Next'
-  }
+  const buttonStyles = {
+    px: "1.2rem",
+  };
+
+  const SurveyPrevButton = () => {
+    return id > 1 ? (
+      <Button variant="outlined" sx={buttonStyles} onClick={() => prevPage()}>
+        Previous
+      </Button>
+    ) : (
+      ""
+    );
+  };
+
+  const SurveyNextButton = () => {
+    return (
+      <Button
+        variant={Number(id) === 17 ? "contained" : "outlined"}
+        sx={buttonStyles}
+        onClick={() => nextPage()}
+      >
+        {Number(id) === 17 ? "Submit" : "Next"}
+      </Button>
+    );
+  };
+
+  const MoreInfoButton = () => {
+    return id < 16 ? (
+      <Button
+        sx={buttonStyles}
+        variant="contained"
+        color="primary"
+        startIcon={<HelpIcon />}
+        onClick={handleClickOpen}
+      >
+        More Info
+      </Button>
+    ) : ( "" )
+  };
 
   const surveyAutofill = () => {
-    history.push('/survey/17');
+    history.push("/survey/17");
     dispatch({
-      type: 'AUTOFILL_SURVEY',
-      payload: [{
-        "id": 1,
-        "score": -0.50
-      },
-      {
-        "id": 2,
-        "score": -0.75
-      },
-      {
-        "id": 3,
-        "score": -0.75
-      },
-      {
-        "id": 4,
-        "score": 0.25
-      },
-      {
-        "id": 5,
-        "score": -0.25
-      },
-      {
-        "id": 6,
-        "score": -0.5
-      },
-      {
-        "id": 7,
-        "score": -0.5
-      },
-      {
-        "id": 8,
-        "score": -0.75
-      },
-      {
-        "id": 9,
-        "score": 0
-      },
-      {
-        "id": 10,
-        "score": 0.50
-      },
-      {
-        "id": 11,
-        "score": 0
-      },
-      {
-        "id": 12,
-        "score": -0.25
-      },
-      {
-        "id": 13,
-        "score": 0.25
-      },
-      {
-        "id": 14,
-        "score": -0.50
-      },
-      {
-        "id": 15,
-        "score": 0.25
-      },
-      {
-        "id": 16,
-        "score": -1
-      },
-      {
-        "id": 17,
-        "score": -1
-      }]
-    })
-  }
+      type: "AUTOFILL_SURVEY",
+      payload: [
+        {
+          id: 1,
+          score: -0.5,
+        },
+        {
+          id: 2,
+          score: -0.75,
+        },
+        {
+          id: 3,
+          score: -0.75,
+        },
+        {
+          id: 4,
+          score: 0.25,
+        },
+        {
+          id: 5,
+          score: -0.25,
+        },
+        {
+          id: 6,
+          score: -0.5,
+        },
+        {
+          id: 7,
+          score: -0.5,
+        },
+        {
+          id: 8,
+          score: -0.75,
+        },
+        {
+          id: 9,
+          score: 0,
+        },
+        {
+          id: 10,
+          score: 0.5,
+        },
+        {
+          id: 11,
+          score: 0,
+        },
+        {
+          id: 12,
+          score: -0.25,
+        },
+        {
+          id: 13,
+          score: 0.25,
+        },
+        {
+          id: 14,
+          score: -0.5,
+        },
+        {
+          id: 15,
+          score: 0.25,
+        },
+        {
+          id: 16,
+          score: -1,
+        },
+        {
+          id: 17,
+          score: -1,
+        },
+      ],
+    });
+  };
 
   return (
     <section id="survey-body">
       <SurveyIntro />
 
       <section id="survey-body-question">
-        <h4
-          onClick={surveyAutofill}
-        >
-          Question {id}: {surveyQuestion[Number(id) - 1]?.question}
-        </h4>
-        {id < 16 &&
-        <div className="btn more-info" onClick={handleClickOpen}>
-          <HelpIcon />More Info
-        </div>}
+        <div onClick={surveyAutofill}>
+          {/* onClick for DEMO PURPOSES ONLY */}
+          <Heading3>Question {id}:</Heading3>
+          <Heading3 fontSx={{ fontWeight: 400 }}>
+            {surveyQuestion[Number(id) - 1]?.question}
+          </Heading3>
+        </div>
+
+        <MoreInfoButton />
+
         <SurveyOptions page={id} />
       </section>
+
       <Dialog
         open={open}
         onClose={handleClose}
@@ -153,23 +196,16 @@ export default function SurveyPage() {
         <DialogTitle id="alert-dialog-title">
           {surveyQuestion[Number(id) - 1]?.question}
         </DialogTitle>
-        <DialogContent>
+        <DialogContent sx={{ mb: "1.2rem" }}>
           <DialogContentText id="alert-dialog-description">
             {surveyQuestion[Number(id) - 1]?.caption}
           </DialogContentText>
         </DialogContent>
       </Dialog>
       <div className="survey-previous-next">
-        {id > 1 && (
-          <div className="survey-btn" onClick={() => prevPage()}>
-            {' '}
-            <SurveyPrevButton />
-          </div>
-        )}
-        <div className="survey-btn" onClick={() => nextPage()}>
-          <SurveyNextButton text={nextBtnText()} />
-        </div>
+        <SurveyPrevButton />
+        <SurveyNextButton />
       </div>
     </section>
-  )
+  );
 }
