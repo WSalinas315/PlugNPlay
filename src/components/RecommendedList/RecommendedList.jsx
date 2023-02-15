43782f125bb1a55992ef06508bcd7816dfbf6cb2
August McAllister <august.chm@gmail.com>
2023-02-15 13:42:59 -0600

    feat: comments on recommended list

--- src/components/RecommendedList/RecommendedList.jsx
+++ src/components/RecommendedList/RecommendedList.jsx
@@ -1,11 +1,16 @@
+// HOOK IMPORTS
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useRecommendations } from "../../hooks/storeHooks";
+
+// COMPONENT IMPORTS
 import Loading from "../Loading/Loading";
 import SwipeBox from "../SwipeBox/SwipeBox";
-import { Card } from "@mui/material";
 import Heading1 from "../Headings/Heading1";
-import ParagraphText from "../ParagraphText/ParagraphText";
+
+// MUI IMPORTS
+import { Card } from "@mui/material";
+
 
 export default function RecommendedList() {
   const dispatch = useDispatch();
@@ -15,6 +20,8 @@ export default function RecommendedList() {
 
   const checkForRecs = (refresh = false) => {
     clearGameData();
+
+    // Only fetches new recommendations when explicitly asked, or if recommendations are empty
     if (refresh || recommendations.length === 0) {
       dispatch({ type: "RAWG/FETCH_RECOMMENDATIONS" })
     } else return;
@@ -22,9 +29,13 @@ export default function RecommendedList() {
 
   useEffect(checkForRecs, []);
 
+  const cardStyling = {
+    border: "1px solid", borderColor: "#dddddd", py: "1.5rem", maxWidth: '100%'
+  }
+
   return recommendations.length > 0 ? (
     <div className="center-on-page">
-      <Card sx={{ border: "1px solid", borderColor: "#dddddd", py: "1.5rem", maxWidth: '100%' }}>
+      <Card sx={cardStyling}>
         <Heading1 sx={{ textAlign: "center" }}>Recommended for You</Heading1>
         <SwipeBox games={recommendations} />
       </Card>
