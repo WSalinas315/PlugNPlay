bcfb5eadbc280ae69ed01f067d4b91eb519adbeb
Victor Llapa <victorllapa@gmail.com>
2023-02-15 13:54:21 -0600

    removed files that had no code within itself.

--- src/components/CollectionGlossary/CollectionGlossary.jsx
+++ src/components/CollectionGlossary/CollectionGlossary.jsx
@@ -1,60 +1,56 @@
-import * as React from "react";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
+import * as React from 'react';
+import Box from '@mui/material/Box';
 
-import Autocomplete from "@mui/material/Autocomplete";
-import TextField from "@mui/material/TextField";
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import GlossaryItem from "../CollectionGlossaryItem/CollectionGlossaryItem";
-import { Card, Typography } from "@mui/material";
-import Heading3 from "../Headings/Heading3";
+import GlossaryItem from '../CollectionGlossaryItem/CollectionGlossaryItem';
+import { Card } from '@mui/material';
+import Heading3 from '../Headings/Heading3';
 
 export default function Glossary() {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const [selectedTerm, setSelectedTerm] = useState("");
+	const [selectedTerm, setSelectedTerm] = useState('');
 
-  const glossary = useSelector((store) => store.glossary.glossary);
-  const terms = glossary.map(item => item.term);
+	const glossary = useSelector(store => store.glossary.glossary);
+	const terms = glossary.map(item => item.term);
 
-  useEffect(() => {
-    dispatch({ type: "GLOSSARY/FETCH" });
-  }, []);
+	useEffect(() => {
+		dispatch({ type: 'GLOSSARY/FETCH' });
+	}, []);
 
-  /**
-   *
-   * @param {object} event This is the event listener from the Autocomplete component.
-   * @param {string} value This is the glossary term from the drop down menu.
-   */
-  const handleChange = (event, value) => {
-    if (terms.includes(value)) {
-      setSelectedTerm(value);
-      dispatch({
-        type: "GLOSSARY/SET_ITEM",
-        payload: value, //This is the term that is was clicked on from the drop down menu.
-      });
-    }
-  };
+	/**
+	 *
+	 * @param {object} event This is the event listener from the Autocomplete component.
+	 * @param {string} value This is the glossary term from the drop down menu.
+	 */
+	const handleChange = (event, value) => {
+		if (terms.includes(value)) {
+			setSelectedTerm(value);
+			dispatch({
+				type: 'GLOSSARY/SET_ITEM',
+				payload: value, //This is the term that is was clicked on from the drop down menu.
+			});
+		}
+	};
 
-  return (
-    <Card>
-      <Box>
-        <Heading3>
-          Select a term from the dropdown to view a brief description.
-        </Heading3>
-        <Autocomplete
-          options={glossary.map(({ term }) => term)}
-          freeSolo //?This will allow suggestions based on input value.
-          renderInput={(params) => (
-            <TextField {...params} label="Search Tags" />
-          )}
-          onInputChange={handleChange}
-        />
-      </Box>
-      <Box>{selectedTerm !== "" &&
-        <GlossaryItem term={selectedTerm} />}</Box>
-    </Card>
-  );
+	return (
+		<Card>
+			<Box>
+				<Heading3>
+					Select a term from the dropdown to view a brief description.
+				</Heading3>
+				<Autocomplete
+					options={glossary.map(({ term }) => term)}
+					freeSolo //?This will allow suggestions based on input value.
+					renderInput={params => <TextField {...params} label='Search Tags' />}
+					onInputChange={handleChange}
+				/>
+			</Box>
+			<Box>{selectedTerm !== '' && <GlossaryItem term={selectedTerm} />}</Box>
+		</Card>
+	);
 }
--- src/components/ViewSettings/ViewSettings.jsx
+++ src/components/ViewSettings/ViewSettings.jsx
@@ -1,62 +1,62 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
-import { Card, Button, IconButton, TextField } from "@mui/material";
-import { Close } from "@mui/icons-material";
+import { Card, Button, IconButton, TextField } from '@mui/material';
+import { Close } from '@mui/icons-material';
 
-import Heading1 from "../Headings/Heading1";
-import Heading3 from "../Headings/Heading3";
-import ParagraphText from "../ParagraphText/ParagraphText";
+import Heading1 from '../Headings/Heading1';
+import Heading3 from '../Headings/Heading3';
+import ParagraphText from '../ParagraphText/ParagraphText';
 
 export default function ViewSettings() {
-  const dispatch = useDispatch();
-  const history = useHistory();
+	const dispatch = useDispatch();
+	const history = useHistory();
 
-  const [ imgUrlIn, setImgUrl ] = useState("");
+	const [imgUrlIn, setImgUrl] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch({ type: 'USER/EDIT_PROFILE_PICTURE', payload: { imgPath: imgUrlIn } })
-    history.push('/profile');
-  };
+	const handleSubmit = event => {
+		event.preventDefault();
+		dispatch({
+			type: 'USER/EDIT_PROFILE_PICTURE',
+			payload: { imgPath: imgUrlIn },
+		});
+		history.push('/profile');
+	};
 
-  return (
-    <div>
-      <Card sx={{ border: '1px solid #dddddd' }}>
-        <div className="corner-buttons">
-          <IconButton
-            onClick={() => history.goBack()}
-          >
-            <Close sx={{ mt: '5px', ml: '5px'}} />
-          </IconButton>
-        </div>
-        <Heading1 sx={{ textAlign: 'center', my: '1rem' }}>Settings</Heading1>
-        <form onSubmit={handleSubmit}>
-          <Heading3 sx={{ mt: "1rem", mb: "0.5rem", ml: '20px' }}>
-            Edit Profile Picture
-          </Heading3>
-          <TextField
-            sx={{ width: "calc(100% - 1rem)", mx: '0.5rem', mb: "0.2rem" }}
-            className="grid-right"
-            value={imgUrlIn}
-            onChange={(e) => setImgUrl(e.target.value)}
-            label="Profile Picture URL"
-          />
-          <ParagraphText sx={{ ml: '20px', mb: '1rem' }} newVariant="detail">
-            Leave blank to use default.
-          </ParagraphText>
-          <div align="center">
-            <Button
-              sx={{ px: '1.2rem', m: "0.3rem" }}
-              variant="outlined"
-              type="submit"
-            >
-              Save Changes
-            </Button>
-          </div>
-        </form>
-      </Card>
-    </div>
-  );
+	return (
+		<div>
+			<Card sx={{ border: '1px solid #dddddd' }}>
+				<div className='corner-buttons'>
+					<IconButton onClick={() => history.goBack()}>
+						<Close sx={{ mt: '5px', ml: '5px' }} />
+					</IconButton>
+				</div>
+				<Heading1 sx={{ textAlign: 'center', my: '1rem' }}>Settings</Heading1>
+				<form onSubmit={handleSubmit}>
+					<Heading3 sx={{ mt: '1rem', mb: '0.5rem', ml: '20px' }}>
+						Edit Profile Picture
+					</Heading3>
+					<TextField
+						sx={{ width: 'calc(100% - 1rem)', mx: '0.5rem', mb: '0.2rem' }}
+						className='grid-right'
+						value={imgUrlIn}
+						onChange={e => setImgUrl(e.target.value)}
+						label='Profile Picture URL'
+					/>
+					<ParagraphText sx={{ ml: '20px', mb: '1rem' }} newVariant='detail'>
+						Leave blank to use default.
+					</ParagraphText>
+					<div align='center'>
+						<Button
+							sx={{ px: '1.2rem', m: '0.3rem' }}
+							variant='outlined'
+							type='submit'>
+							Save Changes
+						</Button>
+					</div>
+				</form>
+			</Card>
+		</div>
+	);
 }
