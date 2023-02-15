9d8ffbe1b261f382b77781b026dc33367cdc7ea1
Victor Llapa <victorllapa@gmail.com>
2023-02-15 14:47:16 -0600

    Added back the useState as it was removed by accident

--- src/components/GameItem/GameItem.jsx
+++ src/components/GameItem/GameItem.jsx
@@ -1,159 +1,154 @@
 // REACT IMPORTS
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useHistory, useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
 
 // HELPER FUNCTION IMPORTS
-import { formatDate } from "../../helpers/dates";
-import { useGameByID } from "../../hooks/storeHooks";
-import { capitalizeFirst } from "../../helpers/words";
+import { formatDate } from '../../helpers/dates';
+import { useGameByID } from '../../hooks/storeHooks';
+import { capitalizeFirst } from '../../helpers/words';
 
 // MODULE IMPORTS
-import Loading from "../Loading/Loading";
-import UserButtons from "./ButtonFilter";
-import MetacriticBadge from "../MetacriticBadge/MetacriticBadge";
-import ParagraphText from "../ParagraphText/ParagraphText";
-import Heading2 from "../Headings/Heading2";
-import "./GameItem.css";
+import Loading from '../Loading/Loading';
+import UserButtons from './ButtonFilter';
+import MetacriticBadge from '../MetacriticBadge/MetacriticBadge';
+import ParagraphText from '../ParagraphText/ParagraphText';
+import Heading2 from '../Headings/Heading2';
+import './GameItem.css';
 
 // MUI IMPORTS
-import { Card, IconButton, Typography } from "@mui/material";
-import { Close } from "@mui/icons-material";
+import { Card, IconButton, Typography } from '@mui/material';
+import { Close } from '@mui/icons-material';
 
 // COMPONENT
 export default function () {
-  const dispatch = useDispatch();
-  const { id } = useParams(); // id of this game
-  const game = useGameByID();
-  const [titleHidden, hideTitle] = useState(false);
+	const dispatch = useDispatch();
+	const { id } = useParams(); // id of this game
+	const game = useGameByID();
+	const [titleHidden, hideTitle] = useState(false);
 
-  // Scroll to top and fetch current game on load
-  useEffect(() => {
-    window.scrollTo(0, 0);
-    dispatch({ type: "RAWG/FETCH_CURRENT_GAME", payload: id });
-  }, []);
+	// Scroll to top and fetch current game on load
+	useEffect(() => {
+		window.scrollTo(0, 0);
+		dispatch({ type: 'RAWG/FETCH_CURRENT_GAME', payload: id });
+	}, []);
 
-  // SEARCH YOUTUBE FOR TRAILERS
-  const YoutubeLink = () => {
-    return (
-      <div>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={`https://www.youtube.com/results?search_query=${game.slug.replaceAll(
-            "-",
-            "+"
-          )}+game+trailer`}
-        >
-          Check out some trailers
-        </a>
-      </div>
-    );
-  };
+	// SEARCH YOUTUBE FOR TRAILERS
+	const YoutubeLink = () => {
+		return (
+			<div>
+				<a
+					target='_blank'
+					rel='noopener noreferrer'
+					href={`https://www.youtube.com/results?search_query=${game.slug.replaceAll(
+						'-',
+						'+'
+					)}+game+trailer`}>
+					Check out some trailers
+				</a>
+			</div>
+		);
+	};
 
-  // METACRITIC BADGE & LINK
-  const MetacriticLink = () => {
-    const metacriticURL =
-      game.metacritic_url !== ""
-        ? game.metacritic_url
-        : `https://www.metacritic.com/search/all/${game.slug.replaceAll(
-            "-",
-            "%20"
-          )}/results`;
+	// METACRITIC BADGE & LINK
+	const MetacriticLink = () => {
+		const metacriticURL =
+			game.metacritic_url !== ''
+				? game.metacritic_url
+				: `https://www.metacritic.com/search/all/${game.slug.replaceAll(
+						'-',
+						'%20'
+				  )}/results`;
 
-    return game.metacritic ? (
-      <MetacriticBadge score={game.metacritic} link={metacriticURL} />
-    ) : (
-      ""
-    );
-  };
+		return game.metacritic ? (
+			<MetacriticBadge score={game.metacritic} link={metacriticURL} />
+		) : (
+			''
+		);
+	};
 
-  // LIST GENRES
-  const Genres = (props) => {
-    return (
-      <Typography variant="h3" {...props}>
-        {game.genres.map((genre) => genre.name).join(", ")}
-      </Typography>
-    );
-  };
+	// LIST GENRES
+	const Genres = props => {
+		return (
+			<Typography variant='h3' {...props}>
+				{game.genres.map(genre => genre.name).join(', ')}
+			</Typography>
+		);
+	};
 
-  // LIST TAGS
-  const Tags = (props) => {
-    return (
-      <ParagraphText newVariant="detail">
-        {"Tags: "}
-        {game.tags.map((tag) => capitalizeFirst(tag.name)).join(", ")}
-      </ParagraphText>
-    );
-  };
+	// LIST TAGS
+	const Tags = props => {
+		return (
+			<ParagraphText newVariant='detail'>
+				{'Tags: '}
+				{game.tags.map(tag => capitalizeFirst(tag.name)).join(', ')}
+			</ParagraphText>
+		);
+	};
 
-  // LIST PLATFORMS
-  const Platforms = (props) => {
-    return (
-      <ParagraphText>
-        {"Platforms: "}
-        <ul>
-          {game.platforms.map(({ platform }) => (
-            <li>{platform.name}</li>
-          ))}
-        </ul>
-      </ParagraphText>
-    );
-  };
+	// LIST PLATFORMS
+	const Platforms = props => {
+		return (
+			<ParagraphText>
+				{'Platforms: '}
+				<ul>
+					{game.platforms.map(({ platform }) => (
+						<li>{platform.name}</li>
+					))}
+				</ul>
+			</ParagraphText>
+		);
+	};
 
-  // SPLASH PAGE, TITLE, CLOSE BUTTON
-  const GameSplashTitle = () => {
-    const history = useHistory();
+	// SPLASH PAGE, TITLE, CLOSE BUTTON
+	const GameSplashTitle = () => {
+		const history = useHistory();
 
-    const backgroundStyling = {
-      backgroundImage: "url(" + game.background_image + ")",
-    };
+		const backgroundStyling = {
+			backgroundImage: 'url(' + game.background_image + ')',
+		};
 
-    const titleClass = titleHidden ? "hidden-title" : "";
+		const titleClass = titleHidden ? 'hidden-title' : '';
 
-    return (
-      <>
-        <div
-          style={backgroundStyling}
-          className={`game-splash-container`}
-        >
-          <Heading2 className="title">{game.name}</Heading2>
-          <div className="close-button">
-            <IconButton
-              onClick={() => history.goBack()}
-              sx={{ color: "#ffffff", p: 0.5 }}
-            >
-              <Close />
-            </IconButton>
-          </div>
-        </div>
-      </>
-    );
-  };
+		return (
+			<>
+				<div style={backgroundStyling} className={`game-splash-container`}>
+					<Heading2 className='title'>{game.name}</Heading2>
+					<div className='close-button'>
+						<IconButton
+							onClick={() => history.goBack()}
+							sx={{ color: '#ffffff', p: 0.5 }}>
+							<Close />
+						</IconButton>
+					</div>
+				</div>
+			</>
+		);
+	};
 
-  /*
+	/*
     COMPONENT EXPORT
   */
-  return game.name ? (
-    <>
-      <Card sx={{ border: "1px solid", borderColor: "#dddddd" }}>
-        <GameSplashTitle />
-        <div className="two-column-grid">
-          <Genres className="grid-left" />
-          <div className="grid-right">
-            <MetacriticLink />
-          </div>
-        </div>
-        <UserButtons game={game} />
-        <YoutubeLink />
-        <Tags />
-        <Platforms />
-        <ParagraphText children={`Released ${formatDate(game.released)}`} />
-        <ParagraphText children={game.description_raw} />
-      </Card>
-      <div className="foot-spacer" />
-    </>
-  ) : (
-    <Loading />
-  );
+	return game.name ? (
+		<>
+			<Card sx={{ border: '1px solid', borderColor: '#dddddd' }}>
+				<GameSplashTitle />
+				<div className='two-column-grid'>
+					<Genres className='grid-left' />
+					<div className='grid-right'>
+						<MetacriticLink />
+					</div>
+				</div>
+				<UserButtons game={game} />
+				<YoutubeLink />
+				<Tags />
+				<Platforms />
+				<ParagraphText children={`Released ${formatDate(game.released)}`} />
+				<ParagraphText children={game.description_raw} />
+			</Card>
+			<div className='foot-spacer' />
+		</>
+	) : (
+		<Loading />
+	);
 }
